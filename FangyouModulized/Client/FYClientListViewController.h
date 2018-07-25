f1c3162dd2bfa874c4889507c8660e250cb02bfc
陈震 <chenzhen@jiaddeMacBook-Pro.local>
2018-07-25 17:15:35 +0800

    init

---
+++ FangyouModulized/Client/FYClientListViewController.h
@@ -0,0 +1,12 @@
+//
+//  FYClientListViewController.h
+//  FangyouModulized
+//
+//  Created by 陈震 on 2018/7/25.
+//
+
+#import "FYBaseViewController.h"
+
+@interface FYClientListViewController : FYBaseViewController
+
+@end
---
+++ FangyouModulized/Common/FYBaseViewController.h
@@ -0,0 +1,12 @@
+//
+//  FYBaseViewController.h
+//  FangyouModulized
+//
+//  Created by 陈震 on 2018/7/25.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface FYBaseViewController : UIViewController
+
+@end
